47ef9c8443aaf44ebbad862ebc2187bdba414e51
hellnomary <maria.porcires@outlook.com>
2023-08-19 17:31:55 +0200

    api

--- Busstops/src/App.tsx
+++ Busstops/src/App.tsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
 import './App.css'
-import { Coords } from './interface';
+import { Coords, Data, StopLocation, Stop } from './interface';
 
 type ReactSetState<T> = React.Dispatch<React.SetStateAction<T>>
 
 function App() {
 
-  const [position, setPosition] = useState<Coords>()
+  const [position, setPosition] = useState<Coords | null>(null)
+  const [station, setStation] = useState<Data | null>(null)
 
-  function getPosition(setPosition: ReactSetState<Coords>) {
+  function getPosition(setPosition: ReactSetState<Coords | null>) { 
     if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition((position: GeolocationPosition) => {
+
         const coords: GeolocationCoordinates = position.coords;
 
         setPosition({lat: coords.latitude, lon: coords.latitude})
@@ -20,10 +22,21 @@ function App() {
     }
   }
 
+  const API_KEY: string ='b4c630ea-b9b0-4bf9-967f-60537212e062';
+
+  async function reverseGeoCode(lat:number, lon: number, setStation) {
+    const url = `https://api.resrobot.se/v2.1/location.nearbystops?originCoordLat=${lat}&originCoordLong=${lon}&format=json&accessId=${API_KEY}`
+    const response = await fetch(url);
+    const data: Data = await response.json()
+    console.log(data.stopLocationOrCoordLocation);
+     setStation(data)
+  }
+
   return (
     <section>
      <button onClick={() => getPosition(setPosition)}>See location</button>
-    <p>Your position is {position?.lat} {position?.lon}</p>
+     <p>Your position is {position?.lat} {position?.lon}</p>
+     <button onClick={() => reverseGeoCode(position.lat, position.lon, setStation)}>Stops</button>
     </section>
   )
 }
--- Busstops/src/interface.d.ts
+++ Busstops/src/interface.d.ts
@@ -3,4 +3,19 @@ interface Coords {
     lon: number;
 }
 
-export type { Coords }
+interface Data {
+    stopLocationOrCoordLocation: StopLocation[]
+  }
+  
+  interface StopLocation {
+    StopLocation: Stop;
+  }
+
+  interface Stop {
+    dist: number;
+    extId: string;
+    name: string;
+  }
+
+
+export type { Coords, Data, StopLocation, Stop }
